bbf3cdeb9045562df92b248acb2c24117dcdaa66
veronical1 <vrluer@gmail.com>
2024-07-13 14:40:08 -0400

    odometry start

--- FtcRobotController-9.2/FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/odometryOnly.java
+++ FtcRobotController-9.2/FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/odometryOnly.java
@@ -1,4 +1,80 @@
 package org.firstinspires.ftc.teamcode.auton;
 
-public class odometryOnly {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "odometryOnly", group = "Iterative OpMode")
+public class odometryOnly extends LinearOpMode {
+
+    //declare motors
+    private DcMotor leftFront;
+    private DcMotor leftBack;
+    private DcMotor rightFront;
+    private DcMotor rightBack;
+
+    //declare encoders
+    private DcMotor leftEncoder;
+    private DcMotor rightEncoder;
+    private DcMotor backEncoder;
+
+    //bot information
+    double trackWidth = 20; //centimeters
+    double trackWidthDelta = 0;
+    double yOffset = -13.5; //centimeters
+    double yOffsetDelta = 0;
+    double leftWheelDiameter = 3.469; //centimeters
+    double rightWheelDiameter = 3.315; //centimeters
+    double backWheelDiameter = 3.471; //centimeters
+    double leftWheelCircumference = Math.PI * leftWheelDiameter;
+    double rightWheelCircumference = Math.PI * rightWheelDiameter;
+    double backWheelCircumference = Math.PI * backWheelDiameter;
+    double countsPerRotation = 8192;
+
+    double [] pose = {0,0, Math.toRadians(270)};
+
+    double previousLeftEncoderPosition = 0;
+    double previousRightEncoderPosition = 0;
+    double previousBackEncoderPosition = 0;
+
+    double adjustablePower;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        //reverse motors
+
+        //reset encoders
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set wheels to run seperate from the encoders
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //connect encoders to the ports
+        leftEncoder = leftFront;
+        rightEncoder = rightFront;
+        backEncoder = rightBack;
+
+        //set start postition - reverse depending the direction the encoder is facing
+        previousLeftEncoderPosition = leftEncoder.getCurrentPosition();
+        previousRightEncoderPosition = rightEncoder.getCurrentPosition();
+        previousBackEncoderPosition = backEncoder.getCurrentPosition();
+
+        waitForStart();
+
+    }
 }
